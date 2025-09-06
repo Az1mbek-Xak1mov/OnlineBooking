2e2c123d9b3eb2f58f4bdf5bdc6748018601ac87
jakhongir161 <usmanovj161>
2025-09-06 18:56:37 +0500

    custom

--- authen/models.py
+++ authen/models.py
@@ -1,76 +1,14 @@
 import uuid
 
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import AbstractUser, UserManager
+from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField, EmailField
 from django.db.models.enums import TextChoices
 from django.db.models.fields import UUIDField
 
+from authen.customs import CustomUserManager
+
 
 # Create your models here.
-
-
-class CustomUserManager(UserManager):
-    use_in_migrations = True
-
-    def _create_user_object(self, phone_number, email, password, **extra_fields):
-        if not phone_number:
-            raise ValueError("The given phone number must be set")
-        user = self.model(phone_number=phone_number, email=email, **extra_fields)
-        user.password = make_password(password)
-        return user
-
-    def _create_user(self, phone_number, email, password, **extra_fields):
-        user = self._create_user_object(phone_number, email, password, **extra_fields)
-        user.save(using=self._db)
-        return user
-
-    async def _acreate_user(self, phone_number, email, password, **extra_fields):
-        user = self._create_user_object(phone_number, email, password, **extra_fields)
-        await user.asave(using=self._db)
-        return user
-
-    def create_user(self, phone_number, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return self._create_user(phone_number, email, password, **extra_fields)
-
-    create_user.alters_data = True
-
-    async def acreate_user(self, phone_number, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", False)
-        extra_fields.setdefault("is_superuser", False)
-        return await self._acreate_user(phone_number, email, password, **extra_fields)
-
-    acreate_user.alters_data = True
-
-    def create_superuser(self, phone_number, email=None, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self._create_user(phone_number, email, password, **extra_fields)
-
-    create_superuser.alters_data = True
-
-    async def acreate_superuser(
-            self, phone_number, email=None, password=None, **extra_fields
-    ):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return await self._acreate_user(phone_number, email, password, **extra_fields)
-
-    acreate_superuser.alters_data = True
 
 
 class User(AbstractUser):
