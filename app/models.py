4a6d3f23feaf7a425a91a3f028f0dbb7f8dcefda
jakhongir161 <usmanovj161>
2025-09-07 14:48:54 +0500

    added

--- app/models.py
+++ app/models.py
@@ -1,12 +1,10 @@
 import uuid
 
 from django.db import models  # noqa
-from django.db.models import CharField, ForeignKey, CASCADE, SET_NULL, PositiveIntegerField, DateField, TimeField, \
-    BooleanField, TextChoices
-from django.db.models.base import Model
+from django.db.models import (CASCADE, SET_NULL, BooleanField, CharField,
+                              DateField, ForeignKey, Model,
+                              PositiveIntegerField, TimeField)
 from django.db.models.fields import DateTimeField, UUIDField
-from rest_framework.fields import ImageField
-
 
 # Create your models here.
 
@@ -18,103 +16,68 @@ class UUIDModel(Model):
         abstract = True
 
 
-class CreatedBaseModel(UUIDModel):
+class CreatedBaseModel(Model):
     updated_at = DateTimeField(auto_now=True)
     created_at = DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
 
-from django.conf import settings
 
-
-class ServiceCategory(Model):
+class ServiceCategory(UUIDModel, CreatedBaseModel):
     name = CharField(max_length=255)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
 
-class Service(Model):
-    user = ForeignKey(settings.AUTH_USER_MODEL,CASCADE,related_name="services")
-    category = ForeignKey(ServiceCategory,SET_NULL,null=True,related_name="services")
+class Service(UUIDModel, CreatedBaseModel):
+    user = ForeignKey('authen.User', CASCADE, related_name="services")
+    category = ForeignKey(ServiceCategory, SET_NULL, null=True, related_name="services")
     name = CharField(max_length=255)
-    location = CharField(max_length=255)
-    capacity = PositiveIntegerField(default=1)  # Bir vaqtning o‘zida necha odam sig‘adi
-    available_places = PositiveIntegerField(default=1)  # Hozirgi mavjud joylar
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
+    address = CharField(max_length=255)
+    capacity = PositiveIntegerField(default=1)
 
     def __str__(self):
-        return f"{self.name} ({self.get_service_type_display()})"
+        return self.name
 
 
-class Schedule(Model):
-
-    service = ForeignKey(Service, CASCADE, related_name="schedules")
+class Schedule(UUIDModel, CreatedBaseModel):
+    service = ForeignKey('app.Service', CASCADE, related_name="schedules")
     date = DateField()
     start_time = TimeField()
     end_time = TimeField()
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.service.name} - {self.date} ({self.start_time}-{self.end_time})"
 
 
-class Calendar(Model):
+class Calendar(UUIDModel, CreatedBaseModel):
     date = DateField(unique=True)
     is_day_off = BooleanField(default=False)
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.date} - {'Dam olish' if self.is_day_off else 'Ish kuni'}"
 
 
-class ServiceSchedule(Model):
+class ServiceSchedule(UUIDModel, CreatedBaseModel):
     service = ForeignKey(Service, CASCADE, related_name="service_schedules")
     schedule = ForeignKey(Schedule, CASCADE, related_name="service_schedules")
-    created_at = DateTimeField(auto_now_add=True)
-    updated_at = DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.service.name} -> {self.schedule.date}"
 
 
-class Booking(Model):
-    service = ForeignKey(Service, CASCADE, related_name="bookings")
-    user = ForeignKey(settings.AUTH_USER_MODEL, CASCADE, related_name="bookings")
-    start_time = DateTimeField()  # Boshlanish vaqti
-    duration = PositiveIntegerField(default=60)  # qancha vaqt (daqiqada)
-    seats = PositiveIntegerField(default=1)  # nechta joy band qildi
-    qr_code = ImageField(upload_to="qrcodes/", blank=True, null=True)  # QR code rasmi
-    created_at = DateTimeField(auto_now_add=True)
-
-    @property
-    def end_time(self):
-        """Boshlanish + duration orqali tugash vaqtini hisoblaydi"""
-        from django.utils import timezone
-        return self.start_time + timezone.timedelta(minutes=self.duration)
-
-    def save(self, *args, **kwargs):
-        import qrcode
-        from io import BytesIO
-        from django.core.files.base import ContentFile
-
-        super().save(*args, **kwargs)  # Avval bookingni saqlash
-
-        if not self.qr_code:
-            data = f"BookingID: {self.id}\nService: {self.service.name}\nTime: {self.start_time}\nSeats: {self.seats}"
-            qr = qrcode.make(data)
-            buffer = BytesIO()
-            qr.save(buffer, format="PNG")
-
-            filename = f"booking_qr_{self.id}.png"
-            self.qr_code.save(filename, ContentFile(buffer.getvalue()), save=False)
-            super().save(update_fields=['qr_code'])
+class Booking(UUIDModel, CreatedBaseModel):
+    service = ForeignKey('app.Service', CASCADE, related_name="bookings")
+    user = ForeignKey('authen.User', CASCADE, related_name="bookings")
+    start_time = DateTimeField()
+    duration = PositiveIntegerField(default=60)
+    seats = PositiveIntegerField(default=1)
 
     def __str__(self):
-        return f"{self.user.username} -> {self.service.name} ({self.start_time.strftime('%H:%M')}, {self.seats} joy)"
+        return (f"{self.user.phone_number} -> "
+                f"{self.service.name} ({self.start_time.strftime('%H:%M')}, {self.seats} joy)")
+
+
+# TODO check models
--- authen/models.py
+++ authen/models.py
@@ -7,12 +7,11 @@ from django.db.models.fields import UUIDField
 
 from authen.customs import CustomUserManager
 
-
 # Create your models here.
 
 
 class User(AbstractUser):
-    id = UUIDField(primary_key=True, default=uuid.uuid4())
+    id = UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
 
     class RoleType(TextChoices):
         CUSTOMER = 'customer', 'Customer'
@@ -31,3 +30,15 @@ class User(AbstractUser):
     @property
     def fullname(self):
         return f"{self.first_name.capitalize()} {self.last_name.capitalize()}"
+
+# class RoleRequest(Model):
+#     id = UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
+#     sender = ForeignKey('authen.User', on_delete=CASCADE, related_name='sender_request')
+#     receiver = ManyToManyField('authen.User', null=True, blank=True, related_name='receiver_request')
+#     message = TextField()
+#     is_read = BooleanField(default=False)
+#     is_accepted = BooleanField(null=True, blank=True)
+#     created_at = DateTimeField(auto_now_add=True)
+#
+#     class Meta:
+#         ordering = 'read_only','-created_at',
