1ed2c21e6ab62fbeca5e26d5ea09347915a41ff6
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-09-10 18:49:57 +0500

    added

---
+++ apps/shared/models.py
@@ -0,0 +1,23 @@
+from django.db.models import Func, Model
+from django.db.models.fields import UUIDField, DateTimeField
+
+
+class GenRandomUUID(Func):
+    function = "gen_random_uuid"
+    template = "%(function)s()"
+    output_field = UUIDField()
+
+
+class UUIDBaseModel(Model):
+    id = UUIDField(primary_key=True, db_default=GenRandomUUID(), editable=False)
+
+    class Meta:
+        abstract = True
+        required_db_vendor = 'postgresql'
+
+class CreatedBaseModel(Model):
+    updated_at = DateTimeField(auto_now=True)
+    created_at = DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
