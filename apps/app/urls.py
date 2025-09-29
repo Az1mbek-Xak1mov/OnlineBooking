2298b7213274510164493861c7dddbbec4bb916d
jakhongir161 <usmanovj161>
2025-09-29 16:22:49 +0500

    added

--- apps/app/urls.py
+++ apps/app/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("category/services/", ServiceCategoryListAPIView.as_view(), name="service-category-list"),
 
     path("services/", ServiceListCreateAPIView.as_view(), name="service-create"),
-    path("services/<uuid:pk>", ServiceDeleteAPIView.as_view(), name="service-delete"),
-    path("services/<uuid:pk>", ServiceRetrieveAPIView.as_view(), name="service-detail"),
+    path("services/delete/<uuid:pk>", ServiceDeleteAPIView.as_view(), name="service-delete"),
+    path("services/detail/<uuid:pk>", ServiceRetrieveAPIView.as_view(), name="service-detail"),
     path("services/my-services/", MyServicesListApiView.as_view(), name="my-service"),
 
     path("bookings/", BookingCreateAPIView.as_view(), name="booking-create"),
