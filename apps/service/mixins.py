0e87f9a204749469b05e5ce596e037de9ff40a17
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-10-03 11:48:48 +0500

    fixed

--- apps/service/mixins.py
+++
@@ -1,6 +0,0 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-
-
-class FilterSearchMixin:
-    filter_backends = (DjangoFilterBackend, SearchFilter)
--- apps/service/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import SAFE_METHODS, BasePermission
-
-
-class IsProvider(BasePermission):
-    def has_permission(self, request, view) -> bool:
-        if request.method in SAFE_METHODS:
-            return True
-        user = request.user
-        return user and user.is_authenticated and user.is_provider
