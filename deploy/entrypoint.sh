1c4352ed9eea6c17fe325e39dd5509319a41f714
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-10-28 00:18:55 +0500

    Use uv run manage.py for migrate and collectstatic in entrypoint (no make required)

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 set -e
 
-make mig
+# Run Django migrations (use uv so we don't depend on make inside the image)
+uv run python manage.py migrate --noinput
 
-# collect static files for admin and others
+# Collect static files so admin assets are available
 uv run python manage.py collectstatic --noinput
 
+# Execute the container CMD (starts gunicorn)
 exec "$@"
